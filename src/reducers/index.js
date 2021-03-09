b3b311f444e21d073dd73dfdeecd6f2aa4adc9d7
Fiona Gunawan <fiona.gunawan5@yahoo.com>
2021-03-08 19:44:03 -0600

    fetching game detail data

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import gamesReducer from "./gamesReducer";
+import detailReducer from "./detailReducer";
 
 //must import combineReducer function to combine all our reducers together into one obj to export into main index.js
 const rootReducer = combineReducers({
   games: gamesReducer,
+  detail: detailReducer,
 });
 
 export default rootReducer;
